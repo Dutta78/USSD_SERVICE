c45972bdb1be5d7a8af8ec19ba675370f860b0f2
Kunal Sharma <kunal0199@gmail.com>
2022-10-09 20:28:17 +0530

    Test apis for connection

---
+++ USSD_API/src/main/java/com/mosippe/ussd_api/Controllers/IndexController.java
@@ -0,0 +1,19 @@
+package com.mosippe.ussd_api.Controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin("*")
+public class IndexController {
+    @GetMapping("")
+    public String test(){
+        return "Hello there! Nice to meet you";
+    }
+    @PostMapping("")
+    public String test1(){
+        return "Hello there! This is post method";
+    }
+}
